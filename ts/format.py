e4414c058c1e666c3d13989c4789d23e6941ba2f
Michele Dalle Rive <dallem@usi.ch>
2023-08-23 17:07:23 +0200

    Update format script to store files in cwd

--- ts/format.py
+++ ts/format.py
@@ -1,17 +1,19 @@
 import sys
+import os
 
-CPU_NAME = "cpu1"
+CPU_NAME = "cpu20"
 
-def format(filename):
-    infile = open(filename, 'r')
-    outfile = open(f"{filename}.out", 'w')
+def format(path):
+    _, filename = os.path.split(path)
+    infile = open(path, 'r')
+    outfile = open(f"./{filename}.out", 'w')
 
     while 1:
         line = infile.readline()
         if not line:
             break
 
-        if CPU_NAME in line:
+        if f"{CPU_NAME}:" in line:
             _, value = line.split(":")
             value = int(value)
             outfile.write(f"{value}\n");
